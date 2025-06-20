561d14e0106e7326e5ae9e86930a85b01f99b02c
Yew12347 <98322702+Yew12347@users.noreply.github.com>
2025-06-20 18:37:08 +0700

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-# Define variables
-URL="https://github.com/Yew12347/installarch/releases/download/alpha02/installarch"
-FILENAME="installarch"
-
 # Download the binary
 echo "Downloading installarch..."
-curl -L -o "$FILENAME" "$URL"
+curl -L -o https://github.com/Yew12347/installarch/releases/download/alpha02/installarch
 
 # Make it executable
 chmod +x "$FILENAME"
